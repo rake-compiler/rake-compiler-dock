0749fbce6dc1c8413311c5b94eab8ce688118309
Lars Kanis <lars@greiz-reinsdorf.de>
2015-06-16 21:48:00 +0200

    Add :verbose option and optionally print command line.

--- lib/rake_compiler_dock.rb
+++ lib/rake_compiler_dock.rb
@@ -1,3 +1,4 @@
+require "shellwords"
 require "rake_compiler_dock/version"
 
 module RakeCompilerDock
@@ -6,18 +7,24 @@ module RakeCompilerDock
   # If a block is given, upon command completion the block is called with an OK flag (true on a zero exit status) and a Process::Status object.
   # Without a block a RuntimeError is raised when the command exits non-zero.
   #
+  # Option +:verbose+ can be set to enable printing of the command line.
+  # If not set, the rake verbose_flag is used.
+  #
   # Examples:
   #
   #   RakeCompilerDock.sh 'bundle && rake cross native gem'
   #
   #   # check exit status after command runs
-  #   sh %{bundle && rake cross native gem} do |ok, res|
+  #   sh %{bundle && rake cross native gem}, verbose: true do |ok, res|
   #     if ! ok
   #       puts "windows cross build failed (status = #{res.exitstatus})"
   #     end
   #   end
-  def sh(cmd, &block)
-    exec('bash', '-c', cmd, &block)
+  def sh(cmd, options={}, &block)
+    if verbose_flag(options)
+      $stderr.puts "rake-compiler-dock bash -c #{ cmd.inspect }"
+    end
+    exec('bash', '-c', cmd, options, &block)
   end
 
   def image_name
@@ -30,10 +37,15 @@ module RakeCompilerDock
   # If a block is given, upon command completion the block is called with an OK flag (true on a zero exit status) and a Process::Status object.
   # Without a block a RuntimeError is raised when the command exits non-zero.
   #
+  # Option +:verbose+ can be set to enable printing of the command line.
+  # If not set, the rake verbose_flag is used.
+  #
   # Examples:
   #
   #   RakeCompilerDock.exec 'bash', '-c', 'echo $RUBY_CC_VERSION'
   def exec(*args)
+    options = (Hash === args.last) ? args.pop : {}
+
     if RUBY_PLATFORM =~ /mingw|mswin/
       # Change Path from "C:\Path" to "/c/Path" as used by boot2docker
       pwd = Dir.pwd.gsub(/^([a-z]):/i){ "/#{$1.downcase}" }
@@ -60,15 +72,25 @@ module RakeCompilerDock
           image_name,
           "sigfw", "runas", *args]
 
+    cmdline = Shellwords.join(cmd)
+    if verbose_flag(options) == true
+      $stderr.puts cmdline
+    end
+
     ok = system(*cmd)
     if block_given?
       yield(ok, $?)
     elsif !ok
-      show_command = cmd.join(" ")
       fail "Command failed with status (#{$?.exitstatus}): " +
-        "[#{show_command}]"
+        "[#{cmdline}]"
     end
   end
 
-  module_function :exec, :sh, :image_name
+  def verbose_flag(options)
+    verbose = options.fetch(:verbose) do
+      Object.const_defined?(:Rake) && Rake.const_defined?(:FileUtilsExt) ? Rake::FileUtilsExt.verbose_flag : false
+    end
+  end
+
+  module_function :exec, :sh, :image_name, :verbose_flag
 end
