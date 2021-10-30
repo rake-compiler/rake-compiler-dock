1e0b8bc99e159d0f7ee41ffb6ececa9b3b1d5d16
Mike Dalessio <mike.dalessio@gmail.com>
2021-10-30 13:37:14 -0400

    test: add coverage for "CI container" use case

--- test/test_environment_variables.rb
+++ test/test_environment_variables.rb
@@ -23,26 +23,10 @@ class TestEnvironmentVariables
       end
     end
 
-    def test_IMAGE
-      assert_equal IMAGE_NAME, rcd_env['RCD_IMAGE']
-    end
-
     def test_RUBY_CC_VERSION
       df = File.read(File.expand_path("../../Dockerfile.mri.erb", __FILE__))
       df =~ /^ENV RUBY_CC_VERSION\s+(.*)\s+$/
       assert_equal $1, rcd_env['RUBY_CC_VERSION']
-    end
-
-    def test_HOST_RUBY_PLATFORM
-      assert_equal RUBY_PLATFORM, rcd_env['RCD_HOST_RUBY_PLATFORM']
-    end
-
-    def test_HOST_RUBY_VERSION
-      assert_equal RUBY_VERSION, rcd_env['RCD_HOST_RUBY_VERSION']
-    end
-
-    def test_PWD
-      assert_equal Dir.pwd, rcd_env['PWD']
     end
 
     def test_RAKE_EXTENSION_TASK_NO_NATIVE
@@ -66,6 +50,30 @@ class TestEnvironmentVariables
     def invocation(command)
       idir = File.join(File.dirname(__FILE__), '../lib')
       "#{RbConfig::CONFIG['RUBY_INSTALL_NAME']} -I#{idir.inspect} bin/rake-compiler-dock bash -c '#{command}'"
+    end
+
+    def test_HOST_RUBY_PLATFORM
+      assert_equal RUBY_PLATFORM, rcd_env['RCD_HOST_RUBY_PLATFORM']
+    end
+
+    def test_HOST_RUBY_VERSION
+      assert_equal RUBY_VERSION, rcd_env['RCD_HOST_RUBY_VERSION']
+    end
+
+    def test_IMAGE
+      assert_equal IMAGE_NAME, rcd_env['RCD_IMAGE']
+    end
+
+    def test_PWD
+      assert_equal Dir.pwd, rcd_env['PWD']
+    end
+  end
+
+  class AsIfContinuousIntegration < Test::Unit::TestCase
+    include Common
+
+    def invocation(command)
+      "docker run -it #{IMAGE_NAME} bash -c '#{command}'"
     end
   end
 end
