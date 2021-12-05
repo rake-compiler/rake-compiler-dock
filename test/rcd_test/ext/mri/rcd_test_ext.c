1b00fdb7133b077180ab3593494c78866736e4bc
Mike Dalessio <mike.dalessio@gmail.com>
2021-12-05 11:51:37 -0500

    test: reproduce issues with missing compiler-rt symbols

--- test/rcd_test/ext/mri/rcd_test_ext.c
+++ test/rcd_test/ext/mri/rcd_test_ext.c
@@ -1,5 +1,9 @@
 #include "rcd_test_ext.h"
 
+#ifndef __has_builtin
+  #define __has_builtin(x) 0
+#endif
+
 VALUE rb_mRcdTest;
 
 static VALUE
@@ -8,9 +12,24 @@ rcdt_do_something(VALUE self)
   return rb_str_new_cstr("something has been done");
 }
 
+static VALUE
+rcdt_darwin_builtin_available_eh(VALUE self)
+{
+#if __has_builtin(__builtin_available)
+  // This version must be higher than MACOSX_DEPLOYMENT_TARGET to prevent clang from optimizing it away
+  if (__builtin_available(macOS 10.14, *)) {
+    return Qtrue;
+  }
+  return Qfalse;
+#else
+  rb_raise(rb_eRuntimeError, "__builtin_available is not defined");
+#endif
+}
+
 void
 Init_rcd_test_ext(void)
 {
   rb_mRcdTest = rb_define_module("RcdTest");
   rb_define_singleton_method(rb_mRcdTest, "do_something", rcdt_do_something, 0);
+  rb_define_singleton_method(rb_mRcdTest, "darwin_builtin_available?", rcdt_darwin_builtin_available_eh, 0);
 }
--- test/rcd_test/test/test_basic.rb
+++ test/rcd_test/test/test_basic.rb
@@ -9,4 +9,14 @@ class TestBasic < Minitest::Test
     assert_equal "something has been done", RcdTest.do_something
   end
 
+  def test_check_darwin_compiler_rt_symbol_resolution
+    skip("jruby should not run libc-specific tests") if RUBY_ENGINE == "jruby"
+
+    if RUBY_PLATFORM.include?("darwin")
+      assert(RcdTest.darwin_builtin_available?)
+    else
+      e = assert_raises(RuntimeError) { RcdTest.darwin_builtin_available? }
+      assert_equal("__builtin_available is not defined", e.message)
+    end
+  end
 end
