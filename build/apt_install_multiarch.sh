ccfaa41bb43b5936eb7cba62ac866a82ec2e13e0
Ian Ker-Seymer <hello@ianks.com>
2022-12-17 16:53:45 -0500

    [linux/arm64] Install both types of pkgs

--- build/apt_install_multiarch.sh
+++ build/apt_install_multiarch.sh
@@ -9,18 +9,19 @@ main() {
 
   shift
 
+  cross_packages=()
   packages=("$@")
 
   if [ "$deb_target_arch" != "$deb_host_arch" ] && [ "$deb_target_arch" != "amd64" ]; then
     echo "Setting up multiarch support for $deb_target_arch" >&2
     dpkg --add-architecture "$deb_target_arch"
-    packages=("${packages[@]/%/:$deb_target_arch}")
+    cross_packages=("${packages[@]/%/:$deb_target_arch}")
   else
     echo "No need to set up multiarch support for $deb_target_arch"
   fi
 
   apt-get -qq -y update
-  apt-get -qq -y install "${packages[@]}"
+  apt-get -qq -y install "${packages[@]}" "${cross_packages[@]}"
   rm -rf /var/lib/apt/lists/*
 
   rm "$0"
