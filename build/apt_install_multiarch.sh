760881374946602ce1288fff679c0b031a676817
Ian Ker-Seymer <hello@ianks.com>
2022-12-17 17:06:32 -0500

    [linux/arm64] Manipulate sources

--- build/apt_install_multiarch.sh
+++ build/apt_install_multiarch.sh
@@ -5,6 +5,7 @@ IFS=$'\n\t'
 
 main() {
   deb_host_arch="$(dpkg --print-architecture)"
+  lsb_release="$(grep VERSION_CODENAME < /etc/os-release | cut -d= -f2 | tr -d '"')"
   deb_target_arch="$1"
 
   shift
@@ -16,6 +17,14 @@ main() {
     echo "Setting up multiarch support for $deb_target_arch" >&2
     dpkg --add-architecture "$deb_target_arch"
     cross_packages=("${packages[@]/%/:$deb_target_arch}")
+
+    # Qualify our current source lists to make sure debian doesn't infer stuff
+    sed -i "s/^deb http/deb [arch=$deb_host_arch] http/" /etc/apt/sources.list
+
+    # Add sources for ported target libs
+    echo "deb [arch=$deb_target_arch] http://ports.ubuntu.com/ubuntu-ports $(lsb_release -cs) main universe restricted multiverse"
+    echo "deb [arch=$deb_target_arch] http://ports.ubuntu.com/ubuntu-ports $(lsb_release -cs)-updates main universe restricted multiverse"
+    echo "deb [arch=$deb_target_arch] http://ports.ubuntu.com/ubuntu-ports $(lsb_release -cs)-security main universe restricted multiverse"
   else
     echo "No need to set up multiarch support for $deb_target_arch"
   fi
