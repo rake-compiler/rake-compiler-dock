b489f4f2005915fe3b3ffbb4345c47274f5d5018
Ian Ker-Seymer <hello@ianks.com>
2022-12-17 16:41:52 -0500

    [linux/arm64] Cleanup multiarch stuff

--- build/apt_install_multiarch.sh
+++ build/apt_install_multiarch.sh
@@ -7,14 +7,21 @@ main() {
   deb_host_arch="$(dpkg --print-architecture)"
   deb_target_arch="$1"
 
+  shift
+
+  packages=("$@")
+
   if [ "$deb_target_arch" != "$deb_host_arch" ]; then
     echo "Setting up multiarch support for $deb_target_arch" >&2
-
     dpkg --add-architecture "$deb_target_arch"
-    apt-get -qq -y update
+    packages=("${packages[@]/%/:$deb_target_arch}")
   else
     echo "No need to set up multiarch support for $deb_target_arch"
   fi
+
+  apt-get -qq -y update
+  apt-get -qq -y install "${packages[@]}"
+  rm -rf /var/lib/apt/lists/*
 
   rm "$0"
 }
