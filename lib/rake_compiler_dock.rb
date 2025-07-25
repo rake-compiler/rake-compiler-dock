66f6ed0ac2e24a71d0e40b8bd718fc1d2ffc921a
Lars Kanis <lars@greiz-reinsdorf.de>
2025-07-25 17:26:52 +0200

    Bump cross rubies to 3.2.9 and 3.3.9

--- lib/rake_compiler_dock.rb
+++ lib/rake_compiler_dock.rb
@@ -83,8 +83,8 @@ module RakeCompilerDock
   #   RakeCompilerDock.cross_rubies
   #   # => {
   #   #      "3.4" => "3.4.5",
-  #   #      "3.3" => "3.3.8",
-  #   #      "3.2" => "3.2.8",
+  #   #      "3.3" => "3.3.9",
+  #   #      "3.2" => "3.2.9",
   #   #      "3.1" => "3.1.7",
   #   #      "3.0" => "3.0.7",
   #   #      "2.7" => "2.7.8",
@@ -93,8 +93,8 @@ module RakeCompilerDock
   def cross_rubies
     {
       "3.4" => "3.4.5",
-      "3.3" => "3.3.8",
-      "3.2" => "3.2.8",
+      "3.3" => "3.3.9",
+      "3.2" => "3.2.9",
       "3.1" => "3.1.7",
       "3.0" => "3.0.7",
       "2.7" => "2.7.8",
@@ -115,10 +115,10 @@ module RakeCompilerDock
   #   # => "3.4.5:2.7.8"
   #
   #   RakeCompilerDock.ruby_cc_version("~> 3.2")
-  #   # => "3.4.5:3.3.8:3.2.8"
+  #   # => "3.4.5:3.3.9:3.2.9"
   #
   #   RakeCompilerDock.ruby_cc_version(Gem::Requirement.new("~> 3.2"))
-  #   # => "3.4.5:3.3.8:3.2.8"
+  #   # => "3.4.5:3.3.9:3.2.9"
   #
   def ruby_cc_version(*requirements)
     cross = cross_rubies
